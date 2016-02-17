c152fd24f6cb1eb8c03d0fcb97c74fa91f7f0756
Luis <Frost_Leviathan@hotmail.com>
2016-02-17 14:27:35 -0700

    Added Admin panel'

---
+++ src/main/java/com/geometrycloud/happydonut/ui/AdminPanel.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2016 Luis Chavez Bustamante
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.geometrycloud.happydonut.ui;
+
+import com.geometrycloud.happydonut.util.UiUtils;
+
+import javax.swing.JPanel;
+
+/**
+ * Panel de administracion.
+ *
+ * @author Luis Chavez Bustamante
+ */
+public class AdminPanel extends JPanel {
+
+    public AdminPanel() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        
+    }
+
+    public static void main(String... args) {
+        UiUtils.launch("Admin", new AdminPanel());
+    }
+}
